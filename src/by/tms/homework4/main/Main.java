7a61d6447b8d64659ea9e8bed16773cb43b91582
Lukyanovich Aleksandr <lukjanvich@gmail.com>
2021-10-12 23:10:06 +0300

    add homework 4

---
+++ src/by/tms/homework4/main/Main.java
@@ -0,0 +1,34 @@
+package by.tms.homework4.main;
+
+import by.tms.homework4.solutions.Point;
+import by.tms.homework4.solutions.Rectangle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(5, 4);
+        Point point3 = new Point(10, 5.5);
+        System.out.printf("Расстояние от точки 1 до точки 3: %.3f\n", point1.getDistance(point3));
+
+        Rectangle rectangle = new Rectangle(point1, point2);
+
+        System.out.println("Площадь прямоугольника: " + rectangle.getArea());
+        System.out.println("Периметр прямоугольника: " + rectangle.getPerimetr());
+        System.out.println("Длина диагонали прямоугольника: " + rectangle.getDiagonalLength());
+
+        Rectangle rectangle1 = new Rectangle();
+
+        point1.setX(-2);
+        point1.setY(-5.259);
+        point2.setX(45.669);
+        point2.setY(-87.336);
+        Rectangle rectangle2 = new Rectangle(point1, point2);
+
+        System.out.printf("Расстояние от точки 1 до точки 3: %.3f\n", point1.getDistance(point3));
+        System.out.printf("Площадь нового прямоугольника: %.3f\n", rectangle2.getArea());
+        System.out.printf("Периметр нового прямоугольника: %.3f\n", rectangle2.getPerimetr());
+        System.out.printf("Длина диагонали нового прямоугольника: %.3f\n", rectangle2.getDiagonalLength());
+    }
+}
---
+++ src/by/tms/homework4/solutions/Rectangle.java
@@ -0,0 +1,28 @@
+package by.tms.homework4.solutions;
+
+public class Rectangle {
+
+    private Point firstPoint;
+    private Point secondPoint;
+
+    public Rectangle(Point firstPoint, Point secondPoint) {
+        this.firstPoint = firstPoint;
+        this.secondPoint = secondPoint;
+    }
+
+    public Rectangle(){
+        System.out.println("Вы не задали параметры прямоугольника");
+    }
+
+    public double getArea() {
+        return Math.abs((secondPoint.getX() - firstPoint.getX()) * (secondPoint.getY() - firstPoint.getY()));
+    }
+
+    public double getPerimetr() {
+        return 2 * (Math.abs(secondPoint.getX() - firstPoint.getX()) + Math.abs(secondPoint.getY()) - firstPoint.getY());
+    }
+
+    public double getDiagonalLength() {
+        return Math.sqrt(Math.pow(secondPoint.getX() - firstPoint.getX(), 2) + Math.pow(secondPoint.getY() - firstPoint.getY(), 2));
+    }
+}
