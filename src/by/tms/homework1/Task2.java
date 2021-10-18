47d6937844e90ef1d7a9308e54caf89753e3c0a0
Lukyanovich Aleksandr <lukjanvich@gmail.com>
2021-10-19 00:51:20 +0300

    fix1 homework1

--- src/by/tms/homework1/Task2.java
+++ src/by/tms/homework1/Task2.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class Task2 {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bведите любое целое положительное число: ");
         int number = scanner.nextInt();
-
         if (number < 0) {
             System.out.println("Вы ввели отрицательное число");
         } else {
             int previousNumber = 0;
             int nextNumber = 1;
             int result = 0;
-
             System.out.println("Все числа Фибоначчи до числа " + number + ": ");
             System.out.print(0 + ", ");
             while (result <= number) {
